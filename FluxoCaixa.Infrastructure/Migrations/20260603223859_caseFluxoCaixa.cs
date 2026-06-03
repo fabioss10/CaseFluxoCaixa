cc7700e878c7fa01e27a9aa41f00fa961b17464c
fabioss10 <fabioss10@hotmail.com>
2026-06-03 19:52:19 -0300

    Esqueleto do projeto pronto

---
+++ FluxoCaixa.Infrastructure/Migrations/20260603223859_caseFluxoCaixa.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FluxoCaixa.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class caseFluxoCaixa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lancamentos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Tipo = table.Column<int>(type: "int", nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lancamentos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OutboxEvents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LancamentoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    EventType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Payload = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ProcessedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OutboxEvents", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lancamentos");
+
+            migrationBuilder.DropTable(
+                name: "OutboxEvents");
+        }
+    }
+}
---
+++ FluxoCaixa.Infrastructure/Persistence/FluxoCaixaDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace FluxoCaixa.Infrastructure.Persistence
+{
+    public class FluxoCaixaDbContextFactory
+    : IDesignTimeDbContextFactory<FluxoCaixaDbContext>
+    {
+        public FluxoCaixaDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<FluxoCaixaDbContext>();
+
+            optionsBuilder.UseSqlServer(
+                "Server=localhost\\SQLEXPRESS;Database=FluxoCaixaDb;Trusted_Connection=True;TrustServerCertificate=True");
+
+            return new FluxoCaixaDbContext(optionsBuilder.Options);
+        }
+    }
+}
